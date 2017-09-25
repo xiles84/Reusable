58847d79c694abe891b3ab4327ff3a62296f6b25
xiles84 <xiles84@gmail.com>
2017-09-25 00:49:15 -0300

    Version 1 with javassist

---
+++ src/main/java/com/tapette/reusables/MockUtils.java
@@ -0,0 +1,175 @@
+package com.tapette.reusables;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.Random;
+import javassist.CannotCompileException;
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.CtMethod;
+import javassist.NotFoundException;
+
+
+public class MockUtils {
+
+	public static <T> CtClass prepareClass(Class<T> clazz, String strMethod , ArrayList<Class<?>> parameters , String newMethos) throws Exception {
+		return prepareClass(clazz.getCanonicalName(), strMethod , parameters , newMethos );
+	}
+
+	public static <T> CtClass prepareClass(String clazz, String strMethod , ArrayList<Class<?>> parameters , String newMethos) throws Exception {
+		Random rn = new Random();
+		ClassPool pool = ClassPool.getDefault();
+		CtClass parentClass = pool.get(clazz);
+		CtClass childClass = pool.makeClass(
+				parentClass.
+				getName()+"Mocker_" + rn.nextInt()%1000,
+				parentClass);
+		//System.out.println(childClass.getName());
+		childClass.setSuperclass(parentClass);
+		childClass.stopPruning(true);
+		childClass.defrost();
+		CtMethod[] ms = parentClass.getDeclaredMethods();
+		for (int i = 0; i < ms.length; i++) {
+			CtMethod method = new CtMethod(ms[i].getReturnType(),
+					ms[i].getName(),
+					ms[i].getParameterTypes(),
+					childClass);
+			method.setBody(ms[i], null);
+			childClass.addMethod(
+					method);
+		}
+		return prepareClass(childClass, strMethod , parameters , newMethos );
+	}
+
+	public boolean hasMethod(CtClass cc , String strMethod , ArrayList<Class<?>> parameters , String newMethos) throws NotFoundException, CannotCompileException {
+		StringBuilder signature = new StringBuilder();
+		signature.append(strMethod).append("(");
+		if(parameters != null) {
+			for (int i = 0; i < parameters.size() - 1; i++) {
+				signature.append(parameters.get(i).getCanonicalName()).append(",");
+			}
+			signature.append(parameters.get(parameters.size()-1).getCanonicalName());
+		}
+		signature.append(")");
+		CtMethod m2 = null;
+		CtMethod[] ms = cc.getSuperclass().getDeclaredMethods();
+		for (int i = 0; i < ms.length; i++) {
+			m2 = new CtMethod(ms[i].getReturnType(), ms[i].getName(), ms[i].getParameterTypes(), cc);
+			//System.out.println(signature.toString() + " xxxx " +ms[i].getLongName().substring(ms[i].getLongName().lastIndexOf("."+strMethod)+1));
+			if(signature.toString().equals(ms[i].getLongName().substring(ms[i].getLongName().lastIndexOf("."+strMethod)+1))) {
+				//System.out.println("fffffffff");
+				//System.out.println("qqqqqqqqqqqqqq " + ms[i].visibleFrom(cc));
+				m2.setBody(newMethos);
+				cc.addMethod(m2);
+			}else {
+				cc.addMethod(m2);
+			}
+		}
+		return false;
+	}
+
+
+	public static <T> CtClass prepareClass(CtClass cc , String strMethod, ArrayList<Class<?>> parameters , String newMethos) throws Exception {
+		StringBuilder signature = new StringBuilder();
+		signature.append(strMethod).append("(");
+		if(parameters != null) {
+			for (int i = 0; i < parameters.size() - 1; i++) {
+				signature.append(parameters.get(i).getCanonicalName()).append(",");
+			}
+			signature.append(parameters.get(parameters.size()-1).getCanonicalName());
+		}
+		signature.append(")");
+		CtMethod[] ms = cc.getDeclaredMethods();
+		for (int i = 0; i < ms.length; i++) {
+			//System.out.println(signature.toString() + " xxXXXXxx " +ms[i].getLongName().substring(ms[i].getLongName().lastIndexOf("."+strMethod)+1));
+			if(signature.toString().equals(ms[i].getLongName().substring(ms[i].getLongName().lastIndexOf("."+strMethod)+1))) {
+				//System.out.println(signature.toString() + " xxxx " +ms[i].getLongName().substring(ms[i].getLongName().lastIndexOf("."+strMethod)+1));
+				ms[i].setBody(newMethos);
+				//System.out.println(ms[i].getReturnType() + " ZZZZ " + ms[i].getName()  + " ZZZZ " + ms[i].getParameterTypes()  + " ZZZZ " + cc);
+				/*CtMethod method = new CtMethod(ms[i].getReturnType(),
+						ms[i].getName(),
+						ms[i].getParameterTypes(),
+						cc);
+				cc.addMethod(
+						method);*/
+			}
+		}
+		return cc;
+	}
+
+
+	/*public static <T> Object initiateClass(CtClass cc, Class<?>[] clazzes, Object[] objects) throws Exception {
+		String[] stringClazzes = new String[clazzes.length];
+		for (int i = 0; i < clazzes.length; i++) {
+			stringClazzes.[i] = clazzes.c
+		}
+		return initiateClass( cc , clazzes , objects);
+	}*/
+
+	public static <T> Object initiateClass(CtClass cc, Class<?>[] clazzes, Object[] objects) throws Exception {
+		cc.stopPruning(false);
+		Class<?> gg = cc.toClass();
+		Object qq = null;
+		boolean verifier = false;
+		try {
+			if(clazzes != null) {
+				for (int i = 0; i < gg.getConstructors().length; i++) {
+					verifier = true;
+					//System.out.println(gg.getConstructors()[i]+ "--" );
+					for (int j = 0; j <gg.getConstructors()[i].getParameters().length; j++) {
+						if(gg.getConstructors()[i].getParameters().length ==  clazzes.length) {
+							//System.out.println(gg.getConstructors()[i].getParameters()[j].getType().getCanonicalName().equalsIgnoreCase(clazzes[j].getCanonicalName()));
+							if(!gg.getConstructors()[i].getParameters()[j].getType().getCanonicalName().equalsIgnoreCase(clazzes[j].getCanonicalName()))
+								verifier = false;
+						}else
+							verifier = false;
+					}
+
+					if(verifier) {
+						return  gg.
+								getConstructors()[i].
+								newInstance(objects);
+					}
+				}
+			}else {
+				//System.out.println(gg.getConstructors()[0]);
+				return gg.
+						newInstance();
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return qq;
+	}
+
+	public static <T> T assignField(T obj, String strField, Object value) throws Exception {
+		Field field = obj.getClass().getDeclaredField(strField);
+		field.setAccessible(true);
+		field.set(obj, value);
+		return obj;
+	}
+
+	public static <T> void assignFinalField(Class<T> clazz, String strField, Object newValue) throws Exception {
+		Field field = clazz.getDeclaredField(strField);
+		field.setAccessible(true);
+		Field modifiersField = Field.class.getDeclaredField("modifiers");
+		modifiersField.setAccessible(true);
+		modifiersField.set(field, field.getModifiers() & ~Modifier.FINAL);
+		field.set(null, newValue);
+	}
+
+	public static <T> void invokeMethod(T obj, String strMethod) throws Exception {
+		Method method = obj.getClass().getDeclaredMethod(strMethod);
+		method.setAccessible(true);
+		method.invoke(obj);
+	}
+
+	public static <T> Object getFieldValue(T obj, String strField) throws Exception {
+		Field field = obj.getClass().getDeclaredField(strField);
+		field.setAccessible(true);
+		return field.get(obj);
+	}
+
+}
---
+++ src/test/java/com/tapette/reusables/MockUtilsTest.java
@@ -0,0 +1,132 @@
+package com.tapette.reusables;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import javassist.CtClass;
+
+public class MockUtilsTest {
+
+	public class TestClass {
+
+		String str = null;
+		int integer = 0;
+		boolean globalFalseBol = false;
+		boolean globalTrueBol = true;
+
+		public TestClass() {}
+
+		public TestClass(String str) {
+			this.str = str;
+		}
+
+		public TestClass(int integer) {
+			this.integer = integer;
+		}
+
+		public TestClass(String str , int integer) {
+			this.str = str;
+			this.integer = integer;
+		}
+
+		public boolean publicMethod() {
+			return privateMethod() && protectedMethod() && justToFill();
+		}
+
+		private boolean privateMethod() {
+			return false;
+		}
+		
+		private boolean privateMethod(boolean var) {
+			return var;
+		}
+
+		protected boolean protectedMethod() {
+			return false;
+		}
+
+		public boolean justToFill() {
+			return true;
+		}
+
+	}
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	public void myCoolTest() throws Exception {
+		CtClass aa = MockUtils.prepareClass("com.tapette.reusables.MockUtilsTest$TestClass", "privateMethod" , null , "{ return true; }");
+		MockUtils.prepareClass(aa, "protectedMethod" , null , "{ return true; }");
+		//MockUtilsTest.TestClass bb = (MockUtilsTest.TestClass)MockUtils.initiateClass(aa , new Class<?>[] {MockUtilsTest.class , String.class} , new Object[] { new MockUtilsTest() , ""});
+		MockUtilsTest.TestClass bb = (MockUtilsTest.TestClass)MockUtils.initiateClass(aa , new Class<?>[] {MockUtilsTest.class} , new Object[] { new MockUtilsTest()});
+		//System.out.println(bb.getClass().getCanonicalName());
+		if(!bb.publicMethod())
+			fail("myCoolTest failed");
+	}
+	
+	@Test
+	public void myCoolTest2() throws Exception {
+		CtClass aa = MockUtils.prepareClass("com.tapette.reusables.MockUtilsTest$TestClass", "privateMethod" , null , "{ return true; }");
+		MockUtils.prepareClass(aa, "protectedMethod" , null , "{ return true; }");
+		MockUtilsTest.TestClass bb = (MockUtilsTest.TestClass)MockUtils.initiateClass(aa , new Class<?>[] { MockUtilsTest.class , String.class } , new Object[] { new MockUtilsTest() , "" });
+		if(!bb.publicMethod())
+			fail("myCoolTest failed");
+	}
+	
+	@Test
+	public void myCoolTest3() throws Exception {
+		CtClass aa = MockUtils.prepareClass("com.tapette.reusables.MockUtilsTest$TestClass", "publicMethod" , null , "{ return true; }");
+		MockUtilsTest.TestClass bb = (MockUtilsTest.TestClass)MockUtils.initiateClass(aa , new Class<?>[] { MockUtilsTest.class } , new Object[] { new MockUtilsTest() });
+		if(!bb.publicMethod())
+			fail("myCoolTest failed");
+	}
+	
+	@Test
+	public void myCoolTest4() throws Exception {
+		CtClass aa = MockUtils.prepareClass("com.tapette.reusables.MockUtilsTest$TestClass", "publicMethod" , null , "{ return privateMethod(true); }");
+		MockUtilsTest.TestClass bb = (MockUtilsTest.TestClass)MockUtils.initiateClass(aa , new Class<?>[] { MockUtilsTest.class } , new Object[] { new MockUtilsTest() });
+		if(!bb.publicMethod())
+			fail("myCoolTest failed");
+	}
+	
+	@Test
+	public void myCoolTest5() throws Exception {
+		CtClass aa1 = MockUtils.prepareClass("com.tapette.reusables.MockUtilsTest$TestClass", "publicMethod" , null , "{ return globalFalseBol; }");
+		MockUtilsTest.TestClass bb1 = (MockUtilsTest.TestClass)MockUtils.initiateClass(aa1 , new Class<?>[] { MockUtilsTest.class } , new Object[] { new MockUtilsTest() });
+		if(bb1.publicMethod())
+			fail("myCoolTest failed");
+		CtClass aa2 = MockUtils.prepareClass("com.tapette.reusables.MockUtilsTest$TestClass", "publicMethod" , null , "{ return globalTrueBol; }");
+		MockUtilsTest.TestClass bb2 = (MockUtilsTest.TestClass)MockUtils.initiateClass(aa2 , new Class<?>[] { MockUtilsTest.class } , new Object[] { new MockUtilsTest() });
+		if(!bb2.publicMethod())
+			fail("myCoolTest failed");
+	}
+	
+	@Test
+	public void myCoolTest6() throws Exception {
+		ArrayList<Class<?>> param =  new ArrayList<>();
+		param.add(boolean.class);
+		CtClass aa = MockUtils.prepareClass("com.tapette.reusables.MockUtilsTest$TestClass", "publicMethod" , null , "{ return privateMethod(false); }");
+		MockUtils.prepareClass(aa, "privateMethod" , param , "{ return $1 ? false : true ; }");
+		MockUtilsTest.TestClass bb = (MockUtilsTest.TestClass)MockUtils.initiateClass(aa , new Class<?>[] { MockUtilsTest.class } , new Object[] { new MockUtilsTest() });
+		if(!bb.publicMethod())
+			fail("myCoolTest failed");
+	}
+	
+	@Test
+	public void myCoolTest7() throws Exception {
+		ArrayList<Class<?>> param =  new ArrayList<>();
+		param.add(boolean.class);
+		CtClass aa = MockUtils.prepareClass("com.tapette.reusables.MockUtilsTest$TestClass", "publicMethod" , null , "{ return privateMethod(false); }");
+		MockUtils.prepareClass(aa, "privateMethod" , param , "{ return !$1; }");
+		MockUtilsTest.TestClass bb = (MockUtilsTest.TestClass)MockUtils.initiateClass(aa , new Class<?>[] { MockUtilsTest.class } , new Object[] { new MockUtilsTest() });
+		if(!bb.publicMethod())
+			fail("myCoolTest failed");
+	}
+
+}
